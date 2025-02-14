abf5e5bacfe626d140a7e4ab08e842fbdd2cad05
PaleAvery <143568240+PaleAvery@users.noreply.github.com>
2025-02-14 14:08:49 +1300

    finished mmf

---
+++ B_01_MMF_v1.py
@@ -0,0 +1,132 @@
+# Functions go here
+def make_statement(statement, decoration):
+    """ Emphasises headings by adding decoration
+     at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+def string_check(question, valid_answers=('yes', 'no'),
+                 num_letters=1):
+    """Checks the users enter the full word
+    or the 'n' letter/s of a worf from a list of valid responses"""
+
+    while True:
+
+        response = input(question).lower()
+
+        for item in valid_answers:
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if its the first letter
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"please choose an option from {valid_answers}")
+
+
+def instructions():
+    make_statement("intructions", "*")
+
+    print('''
+    
+for each ticket holder enter ...
+- their name 
+- their age 
+- the payment method (cash/credit)
+
+the program will record the ticket sale and calculate the
+ticket cost ( and the profit)
+
+Once you have either sold all the tickets or enterned the
+exit code ('xxx'), the program will display the ticket
+sales informations and write the data to a text file 
+
+it will also choose one lucky ticket holder who wins the 
+draw (their ticket is free).
+
+    ''')
+
+
+def not_blank(question):
+    """Checks that a user response is not blank """
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this cant be blank. Please try again.\n")
+
+
+def int_check(question):
+    """Checks users enter an integer that is more than zero (or the 'xxx' exit code)"""
+
+    error = "oops - please enter an integer."
+
+    while True:
+
+        try:
+            response = int(input(question))
+
+            return response
+
+        except ValueError:
+            print(error)
+
+
+# Main routine goes here
+
+# initialize ticket numbers
+MAX_TICKETS = 5
+tickets_sold = 0
+
+# initialise variables / non default options for string checker
+payment_ans = ('cash','credit')
+
+make_statement("Mini-Movie Fundraiser Program", "#")
+
+print()
+want_instructions = string_check("do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+
+while tickets_sold < MAX_TICKETS:
+    # ask user for their name and check its not blank
+    print()
+    name = not_blank("Name: ")
+
+    # if name is exit code break out of loop
+    if name == "xxx":
+        break
+
+        # ask user for their age and check its between 12 and 120
+    age = int_check("Age: ")
+
+    # Output error message and success message
+    if age < 12:
+        print(f"{name} is to young")
+        continue
+    elif age > 120:
+        print(f"{name} is to old")
+        continue
+    else:
+        pass
+
+    # ask user for payment method (cash credit ca and ar)
+    pay_method = string_check("Payment method: ", payment_ans, 2)
+    print(f"{name} has bought a ticket ({pay_method}) ")
+
+tickets_sold += 1
+
+if tickets_sold == MAX_TICKETS:
+    print(f"You have sold all the tickets (ie: {MAX_TICKETS} tickets")
+else:
+    print(f"You have sold {tickets_sold} / {MAX_TICKETS} tickets.")
